2a022841efbc406ee7e74c2b77da20dc84892eb5
XoXo <chirok11@protonmail.ch>
2022-02-17 22:43:29 +0400

    1.0.1

--- index.js
+++ index.js
@@ -7,51 +7,7 @@ let nativeBinding = null
 let localFileExisted = false
 let loadError = null
 
-function isMusl() {
-  // For Node 10
-  if (!process.report || typeof process.report.getReport !== 'function') {
-    try {
-      return readFileSync('/usr/bin/ldd', 'utf8').includes('musl')
-    } catch (e) {
-      return true
-    }
-  } else {
-    const { glibcVersionRuntime } = process.report.getReport().header
-    return !glibcVersionRuntime
-  }
-}
-
 switch (platform) {
-  case 'android':
-    switch (arch) {
-      case 'arm64':
-        localFileExisted = existsSync(join(__dirname, 'dolphin-utilities-rust.android-arm64.node'))
-        try {
-          if (localFileExisted) {
-            nativeBinding = require('./dolphin-utilities-rust.android-arm64.node')
-          } else {
-            nativeBinding = require('dolphin-utilities-rust-android-arm64')
-          }
-        } catch (e) {
-          loadError = e
-        }
-        break
-      case 'arm':
-        localFileExisted = existsSync(join(__dirname, 'dolphin-utilities-rust.android-arm-eabi.node'))
-        try {
-          if (localFileExisted) {
-            nativeBinding = require('./dolphin-utilities-rust.android-arm-eabi.node')
-          } else {
-            nativeBinding = require('dolphin-utilities-rust-android-arm-eabi')
-          }
-        } catch (e) {
-          loadError = e
-        }
-        break
-      default:
-        throw new Error(`Unsupported architecture on Android ${arch}`)
-    }
-    break
   case 'win32':
     switch (arch) {
       case 'x64':
@@ -77,20 +33,6 @@ switch (platform) {
             nativeBinding = require('./dolphin-utilities-rust.win32-ia32-msvc.node')
           } else {
             nativeBinding = require('dolphin-utilities-rust-win32-ia32-msvc')
-          }
-        } catch (e) {
-          loadError = e
-        }
-        break
-      case 'arm64':
-        localFileExisted = existsSync(
-          join(__dirname, 'dolphin-utilities-rust.win32-arm64-msvc.node')
-        )
-        try {
-          if (localFileExisted) {
-            nativeBinding = require('./dolphin-utilities-rust.win32-arm64-msvc.node')
-          } else {
-            nativeBinding = require('dolphin-utilities-rust-win32-arm64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -132,90 +74,17 @@ switch (platform) {
         throw new Error(`Unsupported architecture on macOS: ${arch}`)
     }
     break
-  case 'freebsd':
-    if (arch !== 'x64') {
-      throw new Error(`Unsupported architecture on FreeBSD: ${arch}`)
-    }
-    localFileExisted = existsSync(join(__dirname, 'dolphin-utilities-rust.freebsd-x64.node'))
-    try {
-      if (localFileExisted) {
-        nativeBinding = require('./dolphin-utilities-rust.freebsd-x64.node')
-      } else {
-        nativeBinding = require('dolphin-utilities-rust-freebsd-x64')
-      }
-    } catch (e) {
-      loadError = e
-    }
-    break
   case 'linux':
     switch (arch) {
       case 'x64':
-        if (isMusl()) {
-          localFileExisted = existsSync(
-            join(__dirname, 'dolphin-utilities-rust.linux-x64-musl.node')
-          )
-          try {
-            if (localFileExisted) {
-              nativeBinding = require('./dolphin-utilities-rust.linux-x64-musl.node')
-            } else {
-              nativeBinding = require('dolphin-utilities-rust-linux-x64-musl')
-            }
-          } catch (e) {
-            loadError = e
-          }
-        } else {
-          localFileExisted = existsSync(
-            join(__dirname, 'dolphin-utilities-rust.linux-x64-gnu.node')
-          )
-          try {
-            if (localFileExisted) {
-              nativeBinding = require('./dolphin-utilities-rust.linux-x64-gnu.node')
-            } else {
-              nativeBinding = require('dolphin-utilities-rust-linux-x64-gnu')
-            }
-          } catch (e) {
-            loadError = e
-          }
-        }
-        break
-      case 'arm64':
-        if (isMusl()) {
-          localFileExisted = existsSync(
-            join(__dirname, 'dolphin-utilities-rust.linux-arm64-musl.node')
-          )
-          try {
-            if (localFileExisted) {
-              nativeBinding = require('./dolphin-utilities-rust.linux-arm64-musl.node')
-            } else {
-              nativeBinding = require('dolphin-utilities-rust-linux-arm64-musl')
-            }
-          } catch (e) {
-            loadError = e
-          }
-        } else {
-          localFileExisted = existsSync(
-            join(__dirname, 'dolphin-utilities-rust.linux-arm64-gnu.node')
-          )
-          try {
-            if (localFileExisted) {
-              nativeBinding = require('./dolphin-utilities-rust.linux-arm64-gnu.node')
-            } else {
-              nativeBinding = require('dolphin-utilities-rust-linux-arm64-gnu')
-            }
-          } catch (e) {
-            loadError = e
-          }
-        }
-        break
-      case 'arm':
         localFileExisted = existsSync(
-          join(__dirname, 'dolphin-utilities-rust.linux-arm-gnueabihf.node')
+          join(__dirname, 'dolphin-utilities-rust.linux-x64-gnu.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./dolphin-utilities-rust.linux-arm-gnueabihf.node')
+            nativeBinding = require('./dolphin-utilities-rust.linux-x64-gnu.node')
           } else {
-            nativeBinding = require('dolphin-utilities-rust-linux-arm-gnueabihf')
+            nativeBinding = require('dolphin-utilities-rust-linux-x64-gnu')
           }
         } catch (e) {
           loadError = e
