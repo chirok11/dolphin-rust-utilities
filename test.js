00e4b28f4461274b5a8b47cc0816a26dd137f1df
XoXo <chirok11@protonmail.ch>
2022-03-21 22:27:42 +0400

    1.1.0

--- test.js
+++ test.js
@@ -5,7 +5,8 @@ const path = require('path');
     api.rustLoggerInit();
     console.info(path.join(__dirname, 'data_dir'));
 
-    let re = await api.proxyCheckHttp('209.14.2.75', 9654, '11040', 'a93099efeab8');
+    // 209.14.2.75:6000:11060:2ae704094cf7
+    let re = await api.proxyCheckHttp('209.14.2.75', 6000, '11060', '2ae704094cf7');
     console.debug(re.replaceAll("\n", ''));
     console.info(JSON.parse(re));
 })();
